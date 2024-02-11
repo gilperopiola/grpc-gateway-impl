ccf8e4c03275cf00afa2881ddb17ce52c25514c6
Franco Ferraguti <ferra.main@gmail.com>
2024-02-11 03:06:42 -0300

    v1.0!!!!!

--- pkg/v1/service/service.go
+++ pkg/v1/service/service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"encoding/json"
 	"math/rand"
 
 	usersPB "github.com/gilperopiola/grpc-gateway-impl/pkg/users"
@@ -26,19 +25,6 @@ func NewService() *service {
 	return &service{}
 }
 
-func successfulResponse(data string) *usersPB.ResponseMessage {
-	return &usersPB.ResponseMessage{
-		Success: usersPB.Success_TRUE,
-		Data:    data,
-		Error:   "",
-	}
-}
-
-func structToJSONString(s interface{}) string {
-	str, _ := json.Marshal(s)
-	return string(str)
-}
-
 // Signup should be the implementation of the Signup service method.
 func (s *service) Signup(ctx context.Context, in *usersPB.SignupRequest) (*usersPB.SignupResponse, error) {
 
@@ -48,13 +34,9 @@ func (s *service) Signup(ctx context.Context, in *usersPB.SignupRequest) (*users
 	// 		return entities.SignupResponse{}, fmt.Errorf("error in something(): %w", err)
 	// }
 
-	out := struct {
-		ID int
-	}{
-		ID: rand.Intn(1000),
-	}
-
-	return &usersPB.SignupResponse{Message: successfulResponse(structToJSONString(out))}, nil
+	return &usersPB.SignupResponse{
+		Id: int32(rand.Intn(1000)),
+	}, nil
 }
 
 // Login should be the implementation of the Login service method.
@@ -66,11 +48,7 @@ func (s *service) Login(ctx context.Context, in *usersPB.LoginRequest) (*usersPB
 	// 		return entities.LoginResponse{}, fmt.Errorf("error in something(): %w", err)
 	// }
 
-	out := struct {
-		Token string
-	}{
+	return &usersPB.LoginResponse{
 		Token: "some.jwt.token",
-	}
-
-	return &usersPB.LoginResponse{Message: successfulResponse(structToJSONString(out))}, nil
+	}, nil
 }
