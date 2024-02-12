1d8821a06289daa8143ab29f051f5abfc0f381fd
Franco Ferraguti <ferra.main@gmail.com>
2024-02-12 06:35:30 -0300

    Trying to apply an HTTP error handler

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,10 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
-	"net/textproto"
-	"strings"
 
 	usersPB "github.com/gilperopiola/grpc-gateway-impl/pkg/users"
 	v1 "github.com/gilperopiola/grpc-gateway-impl/pkg/v1"
@@ -19,9 +15,7 @@ import (
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/status"
 )
 
@@ -64,13 +58,9 @@ const (
 // The HTTP Gateway will point towards this server.
 func initGRPCServer(service v1Service.ServiceLayer) *grpc.Server {
 	var (
-		interceptors = grpc.ChainUnaryInterceptor(
-			v1.NewHTTPErrorHandlerInterceptor(),
-			v1.NewValidationInterceptor(),
-		)
-		grpcServer = grpc.NewServer(interceptors)
+		interceptors = v1.GetInterceptors()
+		grpcServer   = grpc.NewServer(interceptors)
 	)
-
 	usersPB.RegisterUsersServiceServer(grpcServer, &v1.API{Service: service})
 	return grpcServer
 }
@@ -94,117 +84,10 @@ func runGRPCServer(grpcServer *grpc.Server) {
 /*             - HTTP -                */
 /* ----------------------------------- */
 
-func errorHandler(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
-
-	const fallback = `{"code": 13, "message": "failed to marshal error message"}` // return Internal when Marshal failed
-
-	var customStatus *runtime.HTTPStatusError
-	if errors.As(err, &customStatus) {
-		err = customStatus.Err
-	}
-
-	s := status.Convert(err)
-	pb := s.Proto()
-
-	w.Header().Del("Trailer")
-	w.Header().Del("Transfer-Encoding")
-
-	contentType := marshaler.ContentType(pb)
-	w.Header().Set("Content-Type", contentType)
-
-	if s.Code() == codes.Unauthenticated {
-		w.Header().Set("WWW-Authenticate", s.Message())
-	}
-
-	buf, merr := marshaler.Marshal(pb)
-	if merr != nil {
-		grpclog.Infof("Failed to marshal error message %q: %v", s, merr)
-		w.WriteHeader(http.StatusInternalServerError)
-		if _, err := io.WriteString(w, fallback); err != nil {
-			grpclog.Infof("Failed to write response: %v", err)
-		}
-		return
-	}
-
-	md, ok := runtime.ServerMetadataFromContext(ctx)
-	if !ok {
-		grpclog.Infof("Failed to extract ServerMetadata from context")
-	}
-
-	handleForwardResponseServerMetadata(w, mux, md)
-
-	// RFC 7230 https://tools.ietf.org/html/rfc7230#section-4.1.2
-	// Unless the request includes a TE header field indicating "trailers"
-	// is acceptable, as described in Section 4.3, a server SHOULD NOT
-	// generate trailer fields that it believes are necessary for the user
-	// agent to receive.
-	doForwardTrailers := requestAcceptsTrailers(r)
-
-	if doForwardTrailers {
-		handleForwardResponseTrailerHeader(w, mux, md)
-		w.Header().Set("Transfer-Encoding", "chunked")
-	}
-
-	st := runtime.HTTPStatusFromCode(s.Code())
-	if customStatus != nil {
-		st = customStatus.HTTPStatus
-	}
-
-	w.WriteHeader(st)
-	if _, err := w.Write(buf); err != nil {
-		grpclog.Infof("Failed to write response: %v", err)
-	}
-
-	if doForwardTrailers {
-		handleForwardResponseTrailer(w, mux, md)
-	}
-}
-
-func handleForwardResponseTrailer(w http.ResponseWriter, mux *runtime.ServeMux, md runtime.ServerMetadata) {
-	for k, vs := range md.TrailerMD {
-		if h, ok := defaultOutgoingTrailerMatcher(k); ok {
-			for _, v := range vs {
-				w.Header().Add(h, v)
-			}
-		}
-	}
-}
-
-func handleForwardResponseServerMetadata(w http.ResponseWriter, mux *runtime.ServeMux, md runtime.ServerMetadata) {
-	for k, vs := range md.HeaderMD {
-		if h, ok := defaultOutgoingHeaderMatcher(k); ok {
-			for _, v := range vs {
-				w.Header().Add(h, v)
-			}
-		}
-	}
-}
-
-func handleForwardResponseTrailerHeader(w http.ResponseWriter, mux *runtime.ServeMux, md runtime.ServerMetadata) {
-	for k := range md.TrailerMD {
-		if h, ok := defaultOutgoingTrailerMatcher(k); ok {
-			w.Header().Add("Trailer", textproto.CanonicalMIMEHeaderKey(h))
-		}
-	}
-}
-
-func defaultOutgoingHeaderMatcher(key string) (string, bool) {
-	return fmt.Sprintf("%s%s", runtime.MetadataHeaderPrefix, key), true
-}
-
-func defaultOutgoingTrailerMatcher(key string) (string, bool) {
-	return fmt.Sprintf("%s%s", runtime.MetadataTrailerPrefix, key), true
-}
-
-func requestAcceptsTrailers(req *http.Request) bool {
-	te := req.Header.Get("TE")
-	return strings.Contains(strings.ToLower(te), "trailers")
-}
-
 // initHTTPGateway initializes the HTTP gateway and registers the API methods there as well.
 // The gateway will point towards the gRPC server's port.
 func initHTTPGateway() *runtime.ServeMux {
-	mux := runtime.NewServeMux(runtime.WithErrorHandler(errorHandler))
+	mux := runtime.NewServeMux(runtime.WithErrorHandler(httpErrorHandler))
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 
 	if err := usersPB.RegisterUsersServiceHandlerFromEndpoint(context.Background(), mux, gRPCPort, opts); err != nil {
@@ -222,4 +105,29 @@ func runHTTPGateway(httpGateway *runtime.ServeMux) {
 	if err := http.ListenAndServe(httpPort, httpGateway); err != nil {
 		log.Fatalf(errMsgServeHTTP, err)
 	}
+}
+
+// httpErrorHandler is a custom error handler for the HTTP gateway. It's pretty simple.
+func httpErrorHandler(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		grpcStatus        = status.Convert(err)
+		httpStatus        = runtime.HTTPStatusFromCode(grpcStatus.Code())
+		httpErrorResponse = httpErrorResponse{Error: grpcStatus.Message()}
+		contentType       = marshaler.ContentType(grpcStatus)
+		buffer            = []byte{}
+	)
+
+	if buffer, err = marshaler.Marshal(httpErrorResponse); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, "")
+		return
+	}
+
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(httpStatus)
+	w.Write(buffer)
+}
+
+type httpErrorResponse struct {
+	Error string `json:"error"`
 }
--- pkg/v1/interceptors.go
+++ pkg/v1/interceptors.go
@@ -3,8 +3,10 @@ package v1
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 
+	"buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	"github.com/bufbuild/protovalidate-go"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -16,6 +18,13 @@ import (
 /*          - Interceptors -           */
 /* ----------------------------------- */
 
+// GetInterceptors returns a gRPC server option that chains all interceptors together.
+func GetInterceptors() grpc.ServerOption {
+	return grpc.ChainUnaryInterceptor(
+		NewValidationInterceptor(),
+	)
+}
+
 // NewValidationInterceptor creates a new *protovalidate.Validator and returns a gRPC interceptor (also executed through HTTP calls)
 // that enforces the validation rules written in the .proto files.
 func NewValidationInterceptor() grpc.UnaryServerInterceptor {
@@ -24,26 +33,34 @@ func NewValidationInterceptor() grpc.UnaryServerInterceptor {
 		log.Fatalf("Failed to create proto validator: %v", err)
 	}
 
-	// This function is the interceptor that will be executed for every gRPC / HTTP call.
-	fn := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		if err := protoValidator.Validate(req.(protoreflect.ProtoMessage)); err != nil {
-			return nil, status.Error(codes.InvalidArgument, err.Error())
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		// If there's no validation error, we call the next handler.
+		err := protoValidator.Validate(req.(protoreflect.ProtoMessage))
+		if err == nil {
+			return handler(ctx, req)
 		}
-		return handler(ctx, req)
-	}
 
-	return fn
+		// If there was an error, we check if it's this type. If it's not, we return a generic error.
+		var validationErr *protovalidate.ValidationError
+		if ok := errors.As(err, &validationErr); !ok {
+			return nil, status.Error(codes.InvalidArgument, "validation error")
+		}
+
+		// If it is, we go through each violation and format the error message accordingly.
+		return nil, status.Error(codes.InvalidArgument, getFormattedErrorMsg(validationErr.ToProto()))
+	}
 }
 
-func NewHTTPErrorHandlerInterceptor() grpc.UnaryServerInterceptor {
-	fn := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		got, err := handler(ctx, req)
+// getFormattedErrorMsg returns a formatted error message based on the validate violations.
+func getFormattedErrorMsg(rulesBroken *validate.Violations) string {
+	formattedErrorMsg := ""
 
-		var validationErr *protovalidate.ValidationError
-		if errors.As(err, &validationErr) {
-			err = validationErr
+	for i, v := range rulesBroken.Violations {
+		formattedErrorMsg += fmt.Sprintf("%s %s", v.FieldPath, v.Message)
+		if i < len(rulesBroken.Violations)-1 {
+			formattedErrorMsg += ", "
 		}
-		return got, err
 	}
-	return fn
+
+	return formattedErrorMsg
 }
